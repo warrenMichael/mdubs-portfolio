fe8eb6181fc186006d0d2bebe928ea75f8f45576
Michael Warren <mdub@michaels-mbp.mynetworksettings.com>
2026-04-08 20:35:13 -0400

    Update basepath to match repo name

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/portfolio',
+  basePath: '/mdubs-portfolio',
   images: {
     unoptimized: true,
   },
